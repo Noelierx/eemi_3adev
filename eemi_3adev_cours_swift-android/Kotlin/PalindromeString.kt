eacf33a4c66ed274930e1cbc52d2fab6f54abc02
Noëlie <noelieroux11@gmail.com>
2019-01-24 18:40:38 +0100

    On pagaye avec le kayak

--- eemi_3adev_cours_swift-android/Kotlin/PalindromeString.kt
+++ eemi_3adev_cours_swift-android/Kotlin/PalindromeString.kt
@@ -4,25 +4,21 @@
 //  Algorithme d'origine : https://github.com/raywenderlich/swift-algorithm-club/tree/master/Palindromes
 //
 
+//Début du palindrome on crée une boucle qui vérifie qu'une lettre est présente en façon "miroir"
+fun IsPalindrome(value: String): Boolean {
+    var result: Boolean = true
+    for ( i in value.length - 1 downTo 0 ) {
+        if ( value[i] != value[ value.length - 1 - i ] ) {
+            result = false
+            break
+        }
+    }
+    return result
+}
+// On ajoute les mots à tester
 fun main() {
     println(String.format("Is Palindrome \"%s\"=%s", "kayak", IsPalindrome("kayak")))
     println(String.format("Is Palindrome \"%s\"=%s", "racecar", IsPalindrome("racecar")))
     println(String.format("Is Palindrome \"%s\"=%s", "abricot", IsPalindrome("abricot")))
     println(String.format("Is Palindrome \"%s\"=%s", "Fraise56", IsPalindrome("fraise56")))
-}
-
-fun IsPalindrome(value: String): Boolean {
-
-    var result: Boolean = true
-
-    for ( i in value.length - 1 downTo 0 ) {
-
-        if ( value[i] != value[ value.length - 1 - i ] ) {
-
-            result = false
-            break
-        }
-    }
-
-    return result
 }
