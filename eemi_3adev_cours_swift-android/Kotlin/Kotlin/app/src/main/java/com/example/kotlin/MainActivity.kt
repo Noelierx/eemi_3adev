3fab384e2e3764bca57a2a7d6caec42e50e13b03
Noëlie <noelieroux11@gmail.com>
2019-03-19 08:57:54 +0100

    Oui oui

--- eemi_3adev_cours_swift-android/Kotlin/Kotlin/app/src/main/java/com/example/kotlin/MainActivity.kt
+++ eemi_3adev_cours_swift-android/Kotlin/Kotlin/app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -3,11 +3,11 @@ package com.example.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
-import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +15,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val label = findViewById<TextView>(R.id.helloLabel)
-        val button = findViewById<Button>(R.id.buttonHello)
+        val siteTitle = findViewById<TextView>(R.id.siteTitle)
+        val getTitle = findViewById<Button>(R.id.getTitle)
 
-        button.setOnClickListener { view ->
-            val t = Thread(Runnable {
-            val u = URL("http://www.perdu.com")
-            val c = OpenConnection()
-            val input = c.getInputStream()
-            val reader = InputStreamReader(input)
-            val buffer = BufferedReader(reader)
+
+        getTitle.setOnClickListener {v ->
+            val url = findViewById<EditText>(R.id.url)
+
+            println(url.text)
+
+            val myThread = Thread(Runnable {
+                val u = URL("https://www.".plus(url.text.toString()))
+                val connection = u.openConnection()
+                val input = connection.getInputStream()
+                val reader = InputStreamReader(input)
+                val buffer = BufferedReader(reader)
+                val s = buffer.readText()
+                val beginning = s.indexOf("<title>")+7
+                val end = s.indexOf("</title>")
+                val sub = if (beginning != -1 ) {
+                    s.substring(beginning, end)
+                } else {
+                    "There is no title"
+                }
+                runOnUiThread {
+                    siteTitle.setText(sub)
+                }
             })
-
-            t.start()
+            myThread.start()
         }
     }
 }
